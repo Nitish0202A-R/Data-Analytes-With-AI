c400fda4f7df054568b688423cde2f97ae8f42bf
Nitish Kumar <nitishsingh2148@gmail.com>
2026-08-25 14:34:17 +0530

    Updated project

--- Python/Nested if & Multiple.py
+++ Python/Nested if & Multiple.py
@@ -49,24 +49,55 @@
 
 #Q2
 
-Reg_no=int(input("Enter you reg no:"))
-if Reg_no==1221:
-    print("right reg no ")
-    Subject=input("Enter you subject:")
-    if Subject.lower()=="python":
-        print("Right subject")
-        password=int(input("Enter you password:"))
-        if password==8888:
-            print("Login successful! Start your exam")
+# Reg_no=int(input("Enter you reg no:"))
+# if Reg_no==1221:
+#     print("right reg no ")
+#     Subject=input("Enter you subject:")
+#     if Subject.lower()=="python":
+#         print("Right subject")
+#         password=int(input("Enter you password:"))
+#         if password==8888:
+#             print("Login successful! Start your exam")
+#         else:
+#             print("Woring password")
+#     else:
+#         print("woring subject")
+# else:
+#     print("worng reg no")
+    
+
+#Q3
+employee = input("Enter your name: ")
+
+if employee.strip() != "":
+    print("Right employee name")
+
+    code = int(input("Enter your code: "))
+
+    if code == 5566:
+        print("Right code")
+
+        department = input("Enter department: ")
+
+        if department.lower() == "finance":
+            print("Entered the right department")
         else:
-            print("Woring password")
+            print("Wrong department")
     else:
-        print("woring subject")
+        print("Wrong code")
 else:
-    print("worng reg no")
+    print("Enter you name")
+    
+                
+
+
     
 
 
+
+
+
+   
 
 
 
--- Python/tempCodeRunnerFile.py
+++ Python/tempCodeRunnerFile.py
@@ -1,6 +1,15 @@
-markes=[56,78,60,75,76]
-# totel=0
-# for m in  markes:
-#     totel += m
-# print("totrl",totel)
-
+Reg_no=int(input("Enter you reg no:"))
+# if Reg_no==1221:
+#     print("right reg no ")
+#     Subject=input("Enter you subject:")
+#     if Subject.lower()=="python":
+#         print("Right subject")
+#         password=int(input("Enter you password:"))
+#         if password==8888:
+#             print("Login successful! Start your exam")
+#         else:
+#             print("Woring password")
+#     else:
+#         print("woring subject")
+# else:
+#     print("worng reg no")
