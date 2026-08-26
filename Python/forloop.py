c925f198c49bdcbaa24b07f6467ef2700cbdb053
Nitish Kumar <nitishsingh2148@gmail.com>
2026-08-26 14:12:08 +0530

    Updated project

--- Python/forloop.py
+++ Python/forloop.py
@@ -50,16 +50,75 @@ for i in range(1,11):
 
 #Q 3 loop in use in if comdition
 
-num=[10,15,20,19,25,30,9,5,6]
-for n in num:
-    if n>10:
-        print(n,"garitdin the number")
+# num=[10,15,20,19,25,30,9,5,6]
+# for n in num:
+#     if n>10:
+#         print(n,"garitdin the number")
 
-number=[0,11]
-for n in number:
-    print(n)
+# number=[0,11]
+# for n in number:
+#     print(n)
 
 # int="hello"
 # for b in range(0,11):
 #     print(int)
 
+# it="hello"
+# for j in range(1,11):
+#     print(j)
+
+
+
+item="SQL"
+for j in item:
+    print(j)
+
+
+name=["pen","apple",'banana']
+for nam in range(1):
+    print(name)
+
+
+for k in range(0,21,2):
+    print(k)
+
+num=[56,98,45]
+totel=0
+for n in num:
+    totel +=n
+print("totel:", totel)
+
+
+
+num2=78,82,90
+totel1=0
+for l in num2:
+    totel1 += l
+print("totel;",totel1)
+
+
+city=["    BIHAR","Pune","MUmbay"]
+cleand=[]
+for c in city:
+    cleand.append(c.strip().title())
+print(cleand)
+
+
+# nu=10,56,15,20,14,17,21,22
+# for j in nu:
+#     if j > 20:
+#         print(j)
+#     else:
+#         print(j)
+
+nu=10,56,15,20,14,17,21,22
+for j in nu:
+    if j % 2 == 0:
+        print(j, "even number")
+    else:
+        print(j, "odd number")
+
+
+id=["EMP56882","EMP585454","EMP58468"]
+for i in id:
+    print(i[-5:])
