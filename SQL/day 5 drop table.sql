f0a3bf011d84964b441c7d05c6c1b9a69bd403c8
Nitish Kumar <nitishsingh2148@gmail.com>
2026-09-10 00:43:43 +0530

    Updated project

--- SQL/day 5 drop table.sql
+++ SQL/day 5 drop table.sql
@@ -29,20 +29,91 @@ DROP TABLE IF EXISTS employee3;
 DROP TABLE IF EXISTS company2;
 
 
-CREATE TABLE users(
+--Drop the table if it already exists
+DROP TABLE IF EXISTS users;
+
+
+--Create the users table
+CREATE TABLE IF NOT EXISTS users(
 User_id INT PRIMARY KEY,
 Name VARCHAR(50),
 Email VARCHAR(100)UNIQUE,
 Age INTEGER CHECK(Age>=18),
-reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 );
 
 
 SELECT * FROM users;
 
-INSERT INTO users(User_id,Name,Email,Age)
+INSERT INTO users(User_id,Name,Email,Age,)
 VALUES
-(2,'Aarit Kumar','aarit1@gmail.com',23)
-(101,'Rohit Kumar','rohit13@gmail.com',21),
-(102,'Ravi Kumar','ravi1@gmail.com',20);
-(1,'Nitish Kumar','nitish12@gmail.com',24),
+(2,'Aarit Kumar','aarit1@gmail.com',23,),
+(101,'Rohit Kumar','rohit13@gmail.com',21,),
+(102,'Ravi Kumar','ravi1@gmail.com',20,);
+
+
+-- INSERTING DATA INTO TABLE
+INSERT INTO users(User_id,Name,Email,Age,)
+VALUES
+(1,'Nitish Kumar','nitish12@gmail.com',24,),
+
+
+CREATE TABLE users(
+    User_id INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Email VARCHAR(100) UNIQUE,
+    Age INTEGER CHECK(Age >= 18),
+    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    City VARCHAR(50)
+);
+
+
+--Insert 5 Sample Users into The Users table
+INSERT INTO users(User_id, Name, Email, Age, City)
+VALUES
+(1,'Nitish Kumar','nitish12@gmail.com',24,'Patna'),
+(2, 'Aarit Kumar', 'aarit1@gmail.com', 23, 'Bihar'),
+(101, 'Rohit Kumar', 'rohit13@gmail.com', 21, 'Saran'),
+(102, 'Ravi Kumar', 'ravi1@gmail.com', 20, 'Chapra');
+
+
+SELECT * FROM users;
+
+-- only choose show the data values
+
+SELECT user_id,city FROM users;
+
+-- Data Update
+UPDATE users
+SET age=25
+WHERE name='Nitish Kumar'
+
+
+UPDATE users
+SET city='chapra'
+WHERE name='Rohit Kumar'
+
+
+UPDATE users
+SET city='saran'
+WHERE age>=20;
+
+
+UPDATE users
+SET city='Bihar',age=24
+WHERE name='Nitish Kumar';
+
+UPDATE users
+SET age=age+1
+WHERE email LIKE 'nitish12@gmail.com'
+
+
+UPDATE users
+SET age=age+1
+WHERE email LIKE '%@gmail.com'
+
+-- How to arange order
+SELECT * FROM users ORDER BY user_id ASC;
+
+
+
